ae6eba1c1b5ba8a19215cd32a046129e19098561
GuidoLorenzetti <guidobruera@gmail.com>
2022-08-27 15:47:42 -0300

    Edited classes.py

---
+++ classes.py
@@ -0,0 +1,14 @@
+class Rabbit:
+
+	def __init__(
+		self, name, weight
+	):
+		self.name = name
+		self.weight = weight
+
+	def eat(self):
+		print(f"{self.name} is eating")
+		self.weight += 0.1
+
+	def noeat(self):
+		self.weight -= 0.2
